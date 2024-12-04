c72556e273124ea0b5c15617bde88b7e6076dcd3
Guillermo Di Donato <61853254+DiDonatoGN@users.noreply.github.com>
2024-12-04 00:40:46 -0300

    Add files via upload

---
+++ dias/dia4.js
@@ -0,0 +1,6 @@
+const backButton = document.getElementById('back-button');
+
+// Redirección al índice
+backButton.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
